1becd6105967bec1977bc5a88f877770188242fd
carloshnp <carloshnpwork@gmail.com>
2023-05-23 21:30:57 -0300

    feat: sign up template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+import { useState } from "react";
 import View from "./components/View";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <View>
-      This is the start.
+      <div className="w-128 h-52 p-6 bg-white rounded-2xl">
+        <h1>Welcome to CodeLeap network!</h1>
+        <form className="flex flex-col">
+          <label htmlFor="name">Please enter your username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="flex justify-end">
+            <button type="submit" className="w-28 h-8 bg-[#7695EC] text-white rounded-lg">ENTER</button>
+          </div>
+        </form>
+        <p>Username: </p>
+      </div>
     </View>
   )
 };
